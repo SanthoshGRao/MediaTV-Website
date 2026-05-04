4afe8da126baec62f2e7442abfb854c4b4e0dc92
Santhosh <santhoshgrao13@gmail.com>
2026-05-04 22:47:05 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Suspense, lazy } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence, MotionConfig } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-
-const Home = lazy(() => import('./pages/Home'));
-const Programs = lazy(() => import('./pages/Programs'));
-const About = lazy(() => import('./pages/About'));
-const Contact = lazy(() => import('./pages/Contact'));
-const Advertise = lazy(() => import('./pages/Advertise'));
+import Home from './pages/Home';
+import Programs from './pages/Programs';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Advertise from './pages/Advertise';
 
 function App() {
   const location = useLocation();
@@ -17,17 +15,15 @@ function App() {
     <MotionConfig reducedMotion="user">
       <div className="min-h-screen bg-dark-600 text-white font-body">
         <Navbar />
-        <Suspense fallback={<main className="pt-28 pb-16 text-center text-white/50">Loading...</main>}>
-          <AnimatePresence mode="wait">
-            <Routes location={location} key={location.pathname}>
-              <Route path="/" element={<Home />} />
-              <Route path="/programs" element={<Programs />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/advertise" element={<Advertise />} />
-            </Routes>
-          </AnimatePresence>
-        </Suspense>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />} />
+            <Route path="/programs" element={<Programs />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/advertise" element={<Advertise />} />
+          </Routes>
+        </AnimatePresence>
         <Footer />
       </div>
     </MotionConfig>
